50f1d4b67605f769fd9e7f7ea0c49c05b5b0d5a9
Raging_Pigeon <farrellrage@gmail.com>
2019-04-23 15:11:50 -0400

    Added templated BubbleSort and InsertionSort

--- BubbleSort.h
+++ BubbleSort.h
@@ -1,6 +1,45 @@
+//------------------------------------------------------------------------------
+//Bubble Sort
+//	Bubble sort iterates through every element in the array, comparing two
+//	consecutive elements at a time and swapping them if the first element
+//	is larger than the second.
+//	Every pass through the array, the last element of the previous pass is
+//	ignored because it will have been "bubbled" up to its correct location.
+//	Sorts in ascending order.
+//------------------------------------------------------------------------------
 #ifndef BubbleSort_h
 #define BubbleSort_h
 
 #include "Main.h"
+
+template <typename type>
+void BubbleSort(type data[], int length)
+{
+	type temp;
+
+	//We will make a pass through the array for every value in the array
+	for (int pass = 0; pass < length; pass++)
+	{
+		//For each element from the beginning of the array to the end of the
+		//current sort range
+		for (int elem = 1; elem < (length - pass); elem++)
+		{
+			//Determine whether the current element is larger than the
+			//next element
+			if (data[elem - 1] > data[elem])
+			{
+				//Save the current element
+				temp = data[elem - 1];
+
+				//Save the next and smaller element into the current
+				//array position
+				data[elem - 1] = data[elem];
+
+				//Save the first and larger element into the next array position
+				data[elem] = temp;
+			} // !if
+		} // !for
+	} // !for
+} // !BubbleSort
 
 #endif // !BubbleSort_h
